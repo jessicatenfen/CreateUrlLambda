273928bb6a5e9eb1930e79a3554a5fbba54abce5
mamaedev <132417242+mamaedev@users.noreply.github.com>
2024-11-28 14:08:01 -0300

    Initial commit: Set up basic project structure for URL shortener using Maven.
    This Java function interacts with an AWS bucket service to store URL data. It generates a unique UUID for each URL and associates the original URL with that UUID for retrieval.

---
+++ src/main/java/com/jessica/lambdaUrl/UrlData.java
@@ -0,0 +1,14 @@
+package com.jessica.lambdaUrl;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
